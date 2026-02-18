749dcf2b959151c1cc1c7890f4baaaf53fa14a8b
Brett Beloin <brettbeloin@gmail.com>
2026-02-17 22:35:57 -0700

    add drop down (fixed)

--- src/main/java/com/csc180/brettbeloin/controllers/startPageController.java
+++ src/main/java/com/csc180/brettbeloin/controllers/startPageController.java
@@ -1,6 +1,9 @@
 package com.csc180.brettbeloin.controllers;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
 
@@ -13,5 +16,13 @@ public class startPageController {
 
     @FXML
     private ComboBox<String> cat;
+
+    @FXML
+    private Button sub;
+
+    @FXML
+    public void submit() throws IOException {
+        System.out.println("OK, OK, you want to submit I get it.");
+    }
 
 }
