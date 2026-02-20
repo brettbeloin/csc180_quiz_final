652afa0904e974a0f67d8f3c0a8353aaefcc31b9
brett beloin <brettbeloin@gmail.com>
2026-02-19 17:21:23 -0700

    add api call

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module com.csc180.brettbeloin {
-    opens com.csc180.brettbeloin to javafx.fxml;
-
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
 
     opens com.csc180.brettbeloin.controllers to javafx.fxml;
+    opens com.csc180.brettbeloin to javafx.fxml;
 
     exports com.csc180.brettbeloin;
     exports com.csc180.brettbeloin.controllers;
